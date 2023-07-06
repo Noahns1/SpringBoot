68fd4feb9d79c68b5ead5b48bba0beb4af18056d
Noahns1 <noahschlaupitz@yahoo.com>
2023-07-05 23:39:09 -0400

    Wrote out all REST API tests, and completed create test

---
+++ src/test/java/com/example/SpringBootApp/APITesting.java
@@ -0,0 +1,57 @@
+package com.example.SpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class APITesting {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldCreate() throws Exception {
+
+        String animalJson = "{\"species\":\"Frog\", \"name\":\"herbert\"}";
+
+        this.mockMvc.perform(post("/addAnimal")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(animalJson))
+                        .andExpect(status().isOk());
+
+
+    }
+
+    @Test
+    public void shouldReadAll() {
+
+    }
+
+    @Test
+    public void shouldReadAllById() {
+
+    }
+
+    @Test
+    public void shouldUpdate() {
+
+    }
+
+    @Test
+    public void shouldDelete() {
+
+    }
+
+
+
+}
