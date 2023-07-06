1d4495a236042d98bc8b3e5476dee7d5e158e635
Noahns1 <noahschlaupitz@yahoo.com>
2023-07-05 22:31:31 -0400

    Added junit tests for reading by Id, updating animal, and deleting animal

--- src/test/java/com/example/SpringBootApp/SpringBootAppApplicationTests.java
+++ src/test/java/com/example/SpringBootApp/SpringBootAppApplicationTests.java
@@ -13,7 +13,7 @@ import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @SpringBootTest
@@ -51,5 +51,55 @@ class SpringBootAppApplicationTests {
 		//assertThat(list).size().isGreaterThan(0);
 		assertTrue("Animal list is greater than zero", list.size() > 0);
 	}
+
+	@Test
+	@Order(3)
+	public void testReadById() {
+
+		long ID = 1;
+
+		Animal animal = animalRepo.findById(ID).get();
+
+		assertEquals("Frog", animal.getSpecies());
+		assertEquals("Herbert", animal.getName());
+
+	}
+
+	@Test
+	@Order(4)
+	public void testUpdate() {
+
+		long ID = 1;
+
+		Animal a = animalRepo.findById(ID).get();
+
+		a.setName("Froppy");
+		animalRepo.save(a);
+
+		assertNotEquals("Herbert", animalRepo.findById(ID).get().getName());
+
+		// To ensure the species doesn't change such as going null
+		assertEquals("Frog", animalRepo.findById(ID).get().getSpecies());
+
+		// To ensure it does equal Froppy
+		assertEquals("Froppy", animalRepo.findById(ID).get().getName());
+
+	}
+
+	@Test
+	@Order(5)
+	public void testDelete() {
+
+		long ID = 1;
+
+		animalRepo.deleteById(ID);
+
+		assertFalse(animalRepo.existsById(ID));
+
+	}
+
+
+
+
 
 }
