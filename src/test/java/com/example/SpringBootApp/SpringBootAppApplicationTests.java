0cdce07e267c84f1d06f02160109f5d9b4ae6b6b
Noahns1 <noahschlaupitz@yahoo.com>
2023-07-05 21:22:55 -0400

    Added junit tests for adding an animal and reading all animals

--- src/test/java/com/example/SpringBootApp/SpringBootAppApplicationTests.java
+++ src/test/java/com/example/SpringBootApp/SpringBootAppApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.SpringBootApp;
 
+import com.example.SpringBootApp.model.Animal;
+import com.example.SpringBootApp.repo.AnimalRepo;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SpringBootAppApplicationTests {
 
+	@Autowired
+	AnimalRepo animalRepo;
+
 	@Test
-	void contextLoads() {
+	@Order(1)
+	public void testCreate() {
+
+		long ID = 1;
+
+		Animal animal = new Animal();
+
+		animal.setId(ID);
+		animal.setSpecies("Frog");
+		animal.setName("Herbert");
+
+		animalRepo.save(animal);
+
+		assertNotNull(animalRepo.findById(ID).get());
+
+		System.out.println("Adding Herbert the Frog worked!");
+
+	}
+
+	@Test
+	@Order(2)
+	public void testReadAll() {
+		List<Animal> list = animalRepo.findAll();
+
+		//assertThat(list).size().isGreaterThan(0);
+		assertTrue("Animal list is greater than zero", list.size() > 0);
 	}
 
 }
